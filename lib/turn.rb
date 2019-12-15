581223d878de23af774b415ea0e3a96b951c7a76
Michael Diaz <mike.diaz006@gmail.com>
2019-12-15 18:15:27 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,21 @@
+def display_board(board)
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+  if index.between?(0,8) && (board[index] != "X" && board[index] != "O")
+    return true
+  end
+end
+
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+def move(board, index, token = "X")
+board[index] = token
+end
